76f3d359ce69bbee2f2f32e5743fb2f8a04d26fc
abhishek <akumar1@sigmoidanalytics.com>
2022-06-08 11:20:15 +0530

    fixing location error

--- utils/get_country_name.py
+++ utils/get_country_name.py
@@ -4,5 +4,5 @@ def get_country_name(place):
     geolocator = Nominatim(user_agent="My-Application")
     location = geolocator.geocode(place, addressdetails=True)
     if location is None:
-        return place
+        return ""
     return location.raw['address']['country']
